31a580fadedb63b73f016b8f546de609d1e9264d
skvic <skvictoria.sk@gmail.com>
2023-12-31 17:06:27 -0500

    [Dynamic Programming] Fibonnacci

---
+++ 509.py
@@ -0,0 +1,20 @@
+'''
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+'''
+
+import numpy as np
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = np.zeros((n+1))
+        if n==0:
+            return 0
+        
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return int(dp[n])
