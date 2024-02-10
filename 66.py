48b684dad18e00f3420d05bde21aee2fefcbbeaf
skvic <skvictoria.sk@gmail.com>
2024-02-10 08:39:23 -0500

    PRevent overflow & list copy error fixed

---
+++ 66.py
@@ -0,0 +1,30 @@
+'''
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+'''
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # we need to prevent overflow
+        res = digits[:]
+        
+        for i in range(0, len(digits)):
+            if (digits[-(i+1)]+1 < 10):
+                res[-(i+1)] += 1
+                return res
+            else:
+                res[-(i+1)] = 0
+        res.insert(0,1)
+                
+        return res
