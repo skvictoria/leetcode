c3909f9bde95b68f147318d99d5f2824c9352de6
skvic <skvictoria.sk@gmail.com>
2024-12-28 00:20:35 -0500

    trasversing

--- 6.py
+++ 6.py
@@ -1,14 +1,27 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        output = s[0]
-        for rowstartIdx in range(numRows):
-            start = s[rowstartIdx]
-            end = rowstartIdx
-            while (end < 2*(numRows-rowstartIdx)):
-                for j in range(rowstartIdx+1, numRows-1):
-                    end += j * 2
-                end += numRows - 1
-                print(end)
-                output += s[end]
-        return output
-        
+        # initialization
+        if numRows == 1:
+            return s
+        rows = [''] * numRows
+        # trasversal
+        curRow = 0
+        down = True
+        for char in s:
+            rows[curRow] += char
+            if down is True:
+                if curRow == numRows - 1:
+                    down = False
+                    curRow -= 1
+                else:
+                    curRow += 1
+            else: # down is False
+                if curRow == 0:
+                    down = True
+                    curRow += 1
+                else:
+                    curRow -= 1
+        output = ''
+        for row in rows:
+            output += row
+        return output 
