69dec89179f9c20d3074a0bcfad2a8e45c9f7112
skvictoria <skvictoria.sk@gmail.com>
2025-11-26 14:30:19 -0500

    add 295.py

---
+++ 295.py
@@ -0,0 +1,34 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+
+        # 1. swap if max(self.small) > min(self.large)
+        if self.small and self.large and -1 * self.small[0] > self.large[0]:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        # 2. swap if len(self.small) > len(self.large) + 1
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        elif len(self.small) +1 < len(self.large):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.small) < len(self.large):
+            return self.large[0]
+        else:
+            return (self.large[0] - self.small[0])/2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
